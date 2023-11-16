ca0eb9b793b1fba9aff38e8e9f479e6514b42efd
Guilherme3712 <guilhermealves3712@gmail.com>
2023-11-16 10:19:46 -0300

    get all

--- src/main/java/com/sistema/helpdesk/resources/exceptions/StandardError.java
+++ src/main/java/com/sistema/helpdesk/resources/exceptions/StandardError.java
@@ -11,7 +11,7 @@ public class StandardError implements Serializable {
     private String message;
     private String path;
 
-    public StandardError(){
+    public StandardError() {
         super();
     }
 
@@ -23,4 +23,45 @@ public class StandardError implements Serializable {
         this.message = message;
         this.path = path;
     }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
 }
